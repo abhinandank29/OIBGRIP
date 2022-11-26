1d0c02805ae38273a74b4845248fa50b3e04f4c6
Abhinandan Kumar <113548113+abhinandank29@users.noreply.github.com>
2022-11-27 01:40:05 +0530

    Add files via upload

---
+++ Task 2/main.js
@@ -0,0 +1,21 @@
+// header scroll effect for navbar
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("header-scrolled");
+    }else{
+        nav.classList.remove("header-scrolled");
+    }
+} 
+
+// nav hide (it will help to close the nav bar after clicking any of it's element in responsive mode) 
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
+ 
+
+
